f08d071fbae1cac22865f79303463a524d072efd
alkarik15 <59719880+alkarik15@users.noreply.github.com>
2020-02-13 17:04:39 +1000

    Auth controller del ResponseEntity

--- src/main/java/ru/skillbox/blog/controller/ApiAuthController.java
+++ src/main/java/ru/skillbox/blog/controller/ApiAuthController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import ru.skillbox.blog.dto.CaptchaDto;
 import ru.skillbox.blog.dto.LoginDto;
@@ -36,9 +37,8 @@ public class ApiAuthController {
     private MailService mailService;
 
     @PostMapping(value = "/register")
-    public ResponseEntity<ResultsDto> apiAuthRegister(@RequestBody UserRegisterDto userRegister) {
-        ResultsDto result = userService.createUser(userRegister);
-        return new ResponseEntity(result, HttpStatus.OK);
+    public ResultsDto apiAuthRegister(@RequestBody UserRegisterDto userRegister) {
+        return userService.createUser(userRegister);
     }
 
     @GetMapping(value = "/captcha")
@@ -50,14 +50,14 @@ public class ApiAuthController {
 
 
     @PostMapping(value = "/restore")
-    public ResponseEntity<ResultsDto> apiPostRestore(HttpServletRequest request, @RequestBody String email) {
-        ResultsDto result = new ResultsDto(mailService.sendEmail(request, email));
-        return new ResponseEntity(result, HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public ResultsDto apiPostRestore(HttpServletRequest request, @RequestBody String email) {
+        return new ResultsDto(mailService.sendEmail(request, email));
     }
 
 
     @PostMapping(value = "/login")
-    public ResponseEntity<ResultLoginDto> apiPostLogin(HttpServletRequest request, @RequestBody LoginDto loginDto) {
+    public ResultLoginDto apiPostLogin(HttpServletRequest request, @RequestBody LoginDto loginDto) {
         ResultLoginDto resultLogin = userService.findUserByEmailAndPassword(loginDto);
         if (resultLogin.getUserLoginDto() != null) {
             resultLogin.setResult(true);
@@ -65,25 +65,25 @@ public class ApiAuthController {
         } else {
             resultLogin.setResult(false);
         }
-        return new ResponseEntity(resultLogin, HttpStatus.OK);
+        return resultLogin;
     }
 
     @GetMapping(value = "/logout")
-    public ResponseEntity<ResultLoginDto> apiGetLogout(HttpServletRequest request) {
+    public ResultLoginDto apiGetLogout(HttpServletRequest request) {
         ResultLoginDto resultLoginDto = new ResultLoginDto();
         if (request.getSession().getAttribute("user") != null) {
             request.getSession().setAttribute("user", "");
         }
         resultLoginDto.setResult(true);
-        return new ResponseEntity(resultLoginDto, HttpStatus.OK);
+        return resultLoginDto;
     }
 
     @GetMapping(value = "/check")
-    public ResponseEntity<ResultLoginDto> apiGetCheck(HttpServletRequest request) {
+    public ResultLoginDto apiGetCheck(HttpServletRequest request) {
         ResultLoginDto resultLoginDto = new ResultLoginDto();
         Integer userId = userService.getUserIdFromSession(request);
         resultLoginDto.setResult(true);
         resultLoginDto.setUserLoginDto(userService.getUserLoginDto(userId));
-        return new ResponseEntity(resultLoginDto, HttpStatus.OK);
+        return resultLoginDto;
     }
 }
